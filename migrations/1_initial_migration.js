a6a25f3645fe7c5074d631550a78454e118d6290
Florian Lenz <flenz81@googlemail.com>
2018-09-27 19:20:23 -0700

    [migration] fixed rinkeby deployment

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,10 +1,14 @@
 const Notary = artifacts.require("Notary");
 const NotaryMulti = artifacts.require("NotaryMulti");
 
-module.exports = function(deployer) {
-  return deployer
-      .deploy(Notary)
-      .then((notary) => {
-        return deployer.deploy(NotaryMulti, notary.address)
-      })
+module.exports = function(deployer, network) {
+
+    if (network === 'rinkeby'){
+        return deployer
+            .deploy(Notary, "0x9441e9bD56d0B632a03b71DC28808b899FAb2570")
+            .then((notary) => {
+                return deployer.deploy(NotaryMulti, notary.address)
+            })
+    }
+
 };
