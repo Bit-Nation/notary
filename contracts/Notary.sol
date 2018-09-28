b398a4b445ebf70f47ae4c8d24889bfba126c6f5
Florian Lenz <flenz81@googlemail.com>
2018-09-27 18:53:21 -0700

    [notary] added notarisation fee

--- contracts/Notary.sol
+++ contracts/Notary.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.23;
 
-contract Notary {
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract Notary is Ownable {
 
     struct Record {
         bytes notarisedData;
@@ -8,6 +10,31 @@ contract Notary {
     }
 
     mapping(bytes32 => Record) public records;
+    uint256 public notarisationFee;
+
+    /**
+    * @dev initialize Notary
+    * @param _owner of the notary
+    */
+    constructor (address _owner) public {
+        owner = _owner;
+    }
+
+    /**
+    * @dev make sure that the call has the notarisation cost
+    */
+    modifier callHasNotarisationCost() {
+        require(msg.value >= notarisationFee);
+        _;
+    }
+
+    /**
+    * @dev set notarisation cost
+    * @param _fee to notarize a record
+    */
+    function setNotarisationFee(uint256 _fee) public onlyOwner {
+        notarisationFee = _fee;
+    }
 
     /**
     * @dev fetch a Record by it's data notarised data
@@ -22,13 +49,22 @@ contract Notary {
     * @dev notarize a new record
     * @param _record the record to notarize
     */
-    function notarize(bytes _record) public {
+    function notarize(bytes _record)
+        public
+        payable
+        callHasNotarisationCost
+    {
 
         // create hash of record to to have an unique and deterministic key
         bytes32 recordHash = keccak256(_record);
 
         // make sure the record hasn't been notarised
         require(records[recordHash].timestamp == 0);
+
+        // transfer notarisation fee to owner
+        if (owner != address(0)){
+            owner.transfer(address(this).balance);
+        }
 
         // notarize record
         records[recordHash] = Record({
--- test/notary.js
+++ test/notary.js
@@ -1,12 +1,58 @@
 const Notary = artifacts.require('Notary');
 const NotaryMulti = artifacts.require('NotaryMulti');
 
-contract('Notary - notarize', () => {
+contract("Notary - constructor ", (accounts) => {
+
+    it('correct state', () => {
+
+        return Notary
+            .new(accounts[5])
+            .then(async (notary) => {
+
+                assert.equal(accounts[5], await notary.owner())
+
+            })
+
+    })
+
+});
+
+contract('Notary - setNotarisationFee', (accounts) => {
+
+    it('only callable by owner', () => {
+
+        const notaryOwner = accounts[5];
+
+        return Notary
+            .new(notaryOwner)
+            .then(async (notary) => {
+
+                await notary.setNotarisationFee(30, {
+                    from: notaryOwner,
+                });
+                assert.equal(30, (await notary.notarisationFee()).toNumber());
+
+                try {
+                    await notary.setNotarisationFee(30);
+                } catch (e) {
+                    assert.equal("VM Exception while processing transaction: revert", e.message);
+                    return
+                }
+
+                assert.fail("expected an error since the call wasn't send my the owner")
+
+            })
+
+    })
+
+});
+
+contract('Notary - notarize', (accounts) => {
 
     it('notarize successfully', () => {
 
         return Notary
-            .new()
+            .new(accounts[8])
             .then(async (notary) => {
 
                 // data to notarize
@@ -23,7 +69,7 @@ contract('Notary - notarize', () => {
     it('notarize twice must throw error', () => {
 
         return Notary
-            .new()
+            .new(accounts[8])
             .then(async (notary) => {
 
                 // data to notarize
@@ -42,16 +88,58 @@ contract('Notary - notarize', () => {
 
             })
 
+    });
+
+    it('should fail on missing notary fee', () => {
+
+        const notaryOwner = accounts[8];
+
+        return Notary
+            .new(notaryOwner)
+            .then(async (notary) => {
+
+                // set fee to 20 wei
+                await notary.setNotarisationFee(20, {
+                    from: notaryOwner
+                });
+
+                const oldOwnerBalance = web3.eth.getBalance(notaryOwner);
+
+                // should work when sending the tx fee
+                await notary.notarize("value", {
+                    value: 20
+                });
+
+                // notary owner must have 20 wei more since the notarisation fee is sent to him
+                assert.equal(
+                    oldOwnerBalance.add(20).toString(),
+                    web3.eth.getBalance(notaryOwner).toString()
+                );
+
+                // shouldn't work when sending too less for the fee
+                try {
+                    await notary.notarize("another value", {
+                        value: 19
+                    });
+                } catch (e) {
+                    assert.equal("VM Exception while processing transaction: revert", e.message);
+                    return;
+                }
+
+                assert.fail("I should fail since value is too low")
+
+            })
+
     })
 
 });
 
-contract("NotaryMulti - notarize", () => {
+contract("NotaryMulti - notarize", (accounts) => {
 
     it('notarize', () => {
 
         return Notary
-            .new()
+            .new(accounts[8])
             .then(async (notary) => {
 
                 const multiNotary = await NotaryMulti.new(notary.address);
